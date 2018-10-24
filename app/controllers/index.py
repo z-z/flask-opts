7f0892da3b15314caeb7e9cc513f890a9fb51438
Zaur <bazzaurik@gmail.com>
2018-10-24 09:16:08 +0300

    add category filter

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 from app.models import Category
+from app.models import Product
 
 
 bp = Blueprint(__name__, __name__, url_prefix='/')
@@ -9,4 +11,12 @@ bp = Blueprint(__name__, __name__, url_prefix='/')
 @bp.route('/')
 def index():
 	categories = Category.query.all()
-	return render_template('index.html', categories=categories)
+	products = Product.query
+
+	category = request.args.get('category', default = None, type = int)
+
+	if category is not None:
+		products = products.filter(Product.category_id.like(category))
+
+	products = products.all()
+	return render_template('index.html', categories=categories, products=products)
