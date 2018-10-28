1fb303b97ac3910b619db841c45e7819e7e38988
Zaur <bazzaurik@gmail.com>
2018-10-28 15:22:35 +0300

    addd price to product, add requirements.txt

--- app/models/Product.py
+++ app/models/Product.py
@@ -3,5 +3,6 @@ from app import db
 class Product(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(200), nullable=False, unique=True)
+	price = db.Column(db.Integer, nullable=False, default=0)
 
 	category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
